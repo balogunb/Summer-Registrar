046d76a75319f5032fe807d85386764062cc3d0b
balogunb <balogunb@lafayette.edu>
2019-06-14 14:17:54 -0400

    first demo working

--- demo.py
+++ demo.py
@@ -3,8 +3,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 
-browser = webdriver.Chrome()
-browser.get('https://automatetheboringstuff.com')
+#browser = webdriver.Chrome()
+#browser.get('https://automatetheboringstuff.com')
 
-browser2 = webdriver.Chrome();
-browser2.get('https://bannerselfservice.lafayette.edu/pls/bprod/bwckschd.p_disp_dyn_sched')
+browser = webdriver.Chrome();
+browser.get('https://bannerselfservice.lafayette.edu/pls/bprod/bwckschd.p_disp_dyn_sched')
+
+
+#selects the summer option
+term = browser.find_element_by_xpath("//option[@value='201840']")
+term.click()
+
+
+#clicks submit
+browser.find_element_by_xpath("//input[@type ='submit']").click()
+
+
+
+
+
+#browser.quit()
