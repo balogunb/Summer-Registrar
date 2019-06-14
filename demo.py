d237eee28c5ddd1f0ef97880a2045d61573faba8
balogunb <balogunb@lafayette.edu>
2019-06-14 13:37:28 -0400

    Headless page navigation and scraping setup

--- demo.py
+++ demo.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
 
 browser = webdriver.Chrome()
 browser.get('https://automatetheboringstuff.com')
+
+browser2 = webdriver.Chrome();
+browser2.get('https://bannerselfservice.lafayette.edu/pls/bprod/bwckschd.p_disp_dyn_sched')
