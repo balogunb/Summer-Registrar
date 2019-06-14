1333a5a9d151858e7af6ab103c32bfdddabe4b8d
balogunb <balogunb@lafayette.edu>
2019-06-14 15:40:11 -0400

    Scraping up next

--- bannerLafayette.py
+++ bannerLafayette.py
@@ -4,9 +4,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 
-#browser = webdriver.Chrome()
-#browser.get('https://automatetheboringstuff.com')
 
+#launch browser and go to website
 browser = webdriver.Chrome();
 browser.get('https://bannerselfservice.lafayette.edu/pls/bprod/bwckschd.p_disp_dyn_sched')
 
@@ -15,28 +14,27 @@ browser.get('https://bannerselfservice.lafayette.edu/pls/bprod/bwckschd.p_disp_d
 term = browser.find_element_by_xpath("//option[@value='201840']")
 term.click()
 
-
 #clicks submit
 browser.find_element_by_xpath("//input[@type ='submit']").click()
 
 
 
 
-#class schedule search page
-#highlight all subject
-
+#class schedule search page, put all contents of select in a list
 subjects = Select(browser.find_element_by_xpath("//select[@name ='sel_subj']"))
-#subjects = browser.find_element_by_xpath("//select[@name ='sel_subj']")
 subjectList = subjects.options
 
+#highlight all subject
 for x in subjectList:
     subjects.select_by_value(x.get_attribute("value"))
     #print(x.get_attribute("value"))
 
 
-#clicks submit
+#clicks submit this leads to the page containing the course schedules
 browser.find_element_by_xpath("//input[@type ='submit']").click()
 
+
+#SCRAPING BEGINS
 
 
 
