77db288b9447857f163236eba972bae4e73f868c
balogunb <balogunb@lafayette.edu>
2019-06-14 13:26:26 -0400

    basic demo of using celenium to launch a headless chrome

--- demo.py
+++ demo.py
@@ -1,1 +1,6 @@
-print("Hello, World!");
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Chrome()
+browser.get('https://automatetheboringstuff.com')
