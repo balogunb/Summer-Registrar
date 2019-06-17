3311ceea612aea0646253df329713cedcd7cbad3
balogunb <balogunb@lafayette.edu>
2019-06-17 17:20:12 -0400

    populated subjectInfo() from both summer periods

--- bannerLafayette.py
+++ bannerLafayette.py
@@ -7,11 +7,11 @@ from bs4 import BeautifulSoup
 
 class SubjectInfo:
     school = 'Lafayette'
-    term ='' #ie summer 2019
+    term ='Summer I 2019' #ie summer 2019
     name = '' #class Name
     code = '' #course code for the subject
     section = '' #course section ID
-    regDate  = ''#registration date
+    regDate  = 'Sep 05,2018 to Jul 05,2019'#registration date
     attribute = '' #course attribute ie Social Science Outcome
     location = '' #where the class is going to hold
     days = '' # days lecture would be held
@@ -37,16 +37,19 @@ class SubjectInfo:
 
 #launch browser and go to website
 browser = webdriver.Chrome();
+browser1 = webdriver.Chrome();
 browser.get('https://bannerselfservice.lafayette.edu/pls/bprod/bwckschd.p_disp_dyn_sched')
-
+browser1.get('https://bannerselfservice.lafayette.edu/pls/bprod/bwckschd.p_disp_dyn_sched')
 
 #selects the summer option
 term = browser.find_element_by_xpath("//option[@value='201840']")
 term.click()
+term1 = browser1.find_element_by_xpath("//option[@value='201850']")
+term1.click()
 
 #clicks submit
 browser.find_element_by_xpath("//input[@type ='submit']").click()
-
+browser1.find_element_by_xpath("//input[@type ='submit']").click()
 
 
 
@@ -54,27 +57,47 @@ browser.find_element_by_xpath("//input[@type ='submit']").click()
 subjects = Select(browser.find_element_by_xpath("//select[@name ='sel_subj']"))
 subjectList = subjects.options
 
+
+subjects1 = Select(browser1.find_element_by_xpath("//select[@name ='sel_subj']"))
+subjectList1 = subjects1.options
+
+
 #highlight all subject
 for x in subjectList:
     subjects.select_by_value(x.get_attribute("value"))
     #print(x.get_attribute("value"))
 
+for x in subjectList1:
+    subjects1.select_by_value(x.get_attribute("value"))
+
 
 #clicks submit this leads to the page containing the course schedules
 browser.find_element_by_xpath("//input[@type ='submit']").click()
-
+browser1.find_element_by_xpath("//input[@type ='submit']").click()
 
 #SCRAPING BEGINS
-soup = BeautifulSoup(browser.page_source)     #beautifulsoup4 if needed
+soup = BeautifulSoup(browser.page_source,'html.parser')     #beautifulsoup4 if needed
 
 
 sectionTag = [] #contains objects which store SubjectInfo
+#sectionTag1 = [] #contains objects which store SubjectInfo
 
-sections =  browser.find_elements_by_xpath("//th[@class='ddtitle']")
-
+titles =  browser.find_elements_by_xpath("//th[@class='ddtitle']")
+titles1 =  browser1.find_elements_by_xpath("//th[@class='ddtitle']")
 
 #Add all subject names to sectionTag
-for x in sections:
+for x in titles:
+    subject = SubjectInfo()
+    details =  x.find_element_by_tag_name('a').text
+    detailsList = details.split(' - ')
+    subject.name = detailsList[0]
+    subject.code = detailsList[1]
+    subject.section = detailsList[2] + " - " + detailsList[3]
+    sectionTag.append(subject)
+
+
+
+for x in titles1:
     subject = SubjectInfo()
     details =  x.find_element_by_tag_name('a').text
     detailsList = details.split(' - ')
@@ -84,7 +107,23 @@ for x in sections:
     sectionTag.append(subject)
 
 for y in sectionTag:
-    print(y.section)
+    print(y.name)
+
+
+#sections = browser.find_elements_by_xpath("/html/body/div/table/tbody/tr/td/table/tbody/tr[2]")
+
+#for y in sections:
+#    print(y.find_element_by_xpath("//td[@class = 'dddefault']").text)
+
+
+#print(len(sections))
+
+
+
+
+    #count++
+
+
 
 
 
