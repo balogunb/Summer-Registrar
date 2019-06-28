3a184782cbaa4db05d3c48967d4d14e549457d88
balogunb <balogunb@lafayette.edu>
2019-06-28 13:10:33 -0400

    fixed inefficient project layout and using env variable for account security

--- courses-website-app/backend/server.js
+++ courses-website-app/backend/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
+var mongoose = require('mongoose');
+require('dotenv').config();
 const PORT = 4000;
 
 app.use(cors());
@@ -12,7 +13,10 @@ app.use(bodyParser.json());
 //console.log('Insert password');
 //var password = prompt("input password");
 
-mongoose.connect('mongodb+srv://Basitb:Adebowale2010@collegecourses-ne1ze.mongodb.net/test?retryWrites=true&w=majority',{useNewURLParser:true });
+
+const server = 'mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@collegecourses-ne1ze.mongodb.net/'+process.env.DB_NAME+'?retryWrites=true&w=majority';
+//console.log(server);
+mongoose.connect(server, {useNewUrlParser: true});
 console.log(mongoose.version);
 const connection = mongoose.connection;
 
