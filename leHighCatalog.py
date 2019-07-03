36bce873d3cab99360b0244bd56243c1257e81d1
balogunb <balogunb@lafayette.edu>
2019-07-02 23:59:53 -0400

    random

--- leHighCatalog.py
+++ leHighCatalog.py
@@ -5,6 +5,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+load_dotenv()
 
 
 class SubjectInfo:
@@ -21,3 +27,25 @@ class SubjectInfo:
     duration = '' #the date range for the class
     type = ''  #usually lecture
     instructor = '' #instructor in charge of the class
+
+'''
+    #make the browser headless
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+
+    #launch browser and go to website
+    browser = webdriver.Chrome(chrome_options=chrome_options);
+'''
+
+
+
+browser = webdriver.Chrome();
+browser.get('https://registration.lehigh.edu/StudentRegistrationSsb/ssb/classSearch/classSearch')
+
+#click submit
+
+#element.sendKeys('2019')
+
+#searchInput = browser.find_element_by_tag_name('input')
+#searchInput.send_keys('2019 Summer Semester')
+#print(browser.find_elements_by_xpath("//li[@id ='select2-result-label-3']").getId())
