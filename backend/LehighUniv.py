dae743f60aa1392d998891a46eb65e72ea92a8b4
Arithima <brownms@lafayette.edu>
2020-06-19 12:20:36 -0400

    My week's changes.
    Opening browser achieved!

--- backend/LehighUniv.py
+++ backend/LehighUniv.py
@@ -12,9 +12,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 class SubjectInfo:
-    school = 'Lafayette College' #School name 
+    school = 'Lehigh University' #School name 
     zipcode = '18042' #School zipcode 
-    term ='Winter/Interim' #ie summer 2019 maybe seperate year and term 
+    term ='Summer' #ie summer 2019 maybe seperate year and term 
     year = '2020'
     department = '' # ie humanities etc 
     courseName = '' #class Name
@@ -33,8 +33,6 @@ class SubjectInfo:
     prereq = ''
     regDeadline = '' #registration deadline
 
-
-
 def getCatalogPage(website):
 
 	#make the browser headless
@@ -49,11 +47,11 @@ def getCatalogPage(website):
 	#browser.get('https://bannerselfservice.lafayette.edu/pls/bprod/bwckschd.p_disp_dyn_sched')
 	#browser1.get('https://bannerselfservice.lafayette.edu/pls/bprod/bwckschd.p_disp_dyn_sched')
 
-	browser = webdriver.Chrome(); # WHY IS THERE A SEMI COLON
+	browser = webdriver.Chrome(); 
 	browser.get(website)
 
 	#selects the term and submit
-	term = browser.find_element_by_xpath("//option[@value='201920']")
+	term = browser.find_element_by_xpath("//option[@value='202020']")
 	term.click()
 	browser.find_element_by_xpath("//input[@type ='submit']").click()
 
@@ -62,73 +60,63 @@ def getCatalogPage(website):
 	subjectList = subjects.options
 
 	#highlight all subject
-	for x in subjectList:
-	    subjects.select_by_value(x.get_attribute("value"))
+	for subject in subjectList:
+	    subjects.select_by_value(subject.get_attribute("value"))
 	    #print(x.get_attribute("value"))
 
 	#clicks submit this leads to the page containing the course schedules
 	browser.find_element_by_xpath("//input[@type ='submit']").click()
 	return browser
 
-
-
-
-#Handles scraping from page holding all course offerings
-def scrapePage(browser):
-	body = browser.find_element_by_xpath("//div[@class ='pagebodydiv']")
-	courseList = body.find_element_by_tag_name('tbody')
-	courseTitles = courseList.find_elements_by_class_name('ddtitle')
-	courseContents = courseList.find_elements_by_xpath("/html/body/div[@class='pagebodydiv']/table[@class='datadisplaytable'][1]/tbody/tr/td")
+    #Handles scraping from page holding all course offerings
+# def scrapePage(browser):
+# 	body = browser.find_element_by_xpath("//div[@class ='pagebodydiv']")
+# 	courseList = body.find_element_by_tag_name('tbody')
+# 	courseTitles = courseList.find_elements_by_class_name('ddtitle')
+# 	courseContents = courseList.find_elements_by_xpath("/html/body/div[@class='pagebodydiv']/table[@class='datadisplaytable'][1]/tbody/tr/td")
 	
-	#print(len(courseTitles))
-	#print(len(courseContents))	
+# 	#print(len(courseTitles))
+# 	#print(len(courseContents))	
 	
-	schList = scrapeInfo(courseTitles,courseContents) #returns a list of course info
-	browser.close()
-	return schList
-	#soup = BeutifulSoup(txt, 'lxml')
+# 	schList = scrapeInfo(courseTitles,courseContents) #returns a list of course info
+# 	browser.close()
+# 	return schList
+# 	#soup = BeutifulSoup(txt, 'lxml')
 
 
 
-def scrapeInfo(titlesList,contentList):
-	browserN = webdriver.Chrome()
-	browserN.implicitly_wait(10)
-	subjectList = [] #contains objects which store SubjectInfo
-	for x in range(len(titlesList)):
-		subj = scrapeTitle(titlesList[x],browserN)
-		#scrapeBody(contentList[x], subj)
-		subjectList.append(subj)
-	#print('list len =' + str(len(subjectList)))
-	return subjectList
+# def scrapeInfo(titlesList,contentList):
+# 	browserN = webdriver.Chrome()
+# 	browserN.implicitly_wait(10)
+# 	subjectList = [] #contains objects which store SubjectInfo
+# 	for x in range(len(titlesList)):
+# 		subj = scrapeTitle(titlesList[x],browserN)
+# 		#scrapeBody(contentList[x], subj)
+# 		subjectList.append(subj)
+# 	#print('list len =' + str(len(subjectList)))
+# 	return subjectList
 
 	
 
 
-# gets course title, CRN and number from the title DOM object
-def scrapeTitle(titleDom,browserN):
-	subject = SubjectInfo() 
-	details = titleDom.find_element_by_tag_name('a')
-	browserN.get(details.get_attribute('href'))
-	getCourseDescription(browserN,subject)
-	#print(subject.description)
-	detailsList = details.text.split(' - ')
-	subject.courseName = detailsList[0]
-	subject.CRN = detailsList[1]
-	subject.section = detailsList[2] + " - " + detailsList[3]
-	return subject
+# # gets course title, CRN and number from the title DOM object
+# def scrapeTitle(titleDom,browserN):
+# 	subject = SubjectInfo() 
+# 	details = titleDom.find_element_by_tag_name('a')
+# 	browserN.get(details.get_attribute('href'))
+# 	getCourseDescription(browserN,subject)
+# 	#print(subject.description)
+# 	detailsList = details.text.split(' - ')
+# 	subject.courseName = detailsList[0]
+# 	subject.CRN = detailsList[1]
+# 	subject.section = detailsList[2] + " - " + detailsList[3]
+# 	return subject
 	
-#can also be modified to collect info on capacity and course price 
-def getCourseDescription(browser,subj):
-	browser.find_element_by_link_text('View Catalog Entry').click()
-	browser.implicitly_wait(10)
-	currInfo = browser.find_element_by_xpath("//td[@class ='ntdefault']").text
-	subj.description = currInfo
-
-
-
-#def scrapeBody(bodyDom,subj):
-	
-
-
+# #can also be modified to collect info on capacity and course price 
+# def getCourseDescription(browser,subj):
+# 	browser.find_element_by_link_text('View Catalog Entry').click()
+# 	browser.implicitly_wait(10)
+# 	currInfo = browser.find_element_by_xpath("//td[@class ='ntdefault']").text
+# 	subj.description = currInfo
 
 
